92c303422f0a5d43846a29a739f641327bc99ade
Bernhard Waldbrunner <vbwx01@gmail.com>
2017-12-28 18:02:02 +0100

    Improve command interface for Makefiles

--- include/jku.mk
+++ include/jku.mk
@@ -1,15 +1,16 @@
 _META := $(HOME)/.pandoc/meta
 _INC := $(HOME)/.pandoc/include
 
+CITEPROC := --filter=pandoc-citeproc
+APA := $(CITEPROC) $(_META)/apa.yaml
+
 pandoc_pdf = pandoc \
+	--read=markdown$(EXT) \
 	--write=latex \
 	--template=jku.tex \
 	--pdf-engine=xelatex \
 	--resource-path=$(_INC):include:. \
 	--output=$@ \
-	$(OPT) \
-	$(OPT_PDF) \
-	$(PREPEND) $(_META)/jku.yaml $+ $(APPEND)
-
-OPT_APA := --csl=apa.csl
-APA := $(_META)/apa.yaml
+	$(OPT) $(OPT_PDF) \
+	$(_META)/jku.yaml $+ \
+	$(APPEND) $(APPEND_PDF)
