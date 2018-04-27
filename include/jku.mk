8922be5dd73a955fadba6e66a6c4b5d429e2ae3c
Bernhard Waldbrunner <vbwx01@gmail.com>
2018-04-27 13:14:14 +0200

    Make global metadata file configurable in makefile

--- include/jku.mk
+++ include/jku.mk
@@ -3,6 +3,7 @@ SHELL := /bin/bash
 _META := $(HOME)/.pandoc/meta
 _INC := $(HOME)/.pandoc/include
 
+META = $(_META)/$(TEMPLATE).yaml
 INC = $(dir $@)include
 RESPATH = $(dir $@):$(INC):$(_INC)
 ENGINE := xelatex
@@ -17,16 +18,14 @@ PDF = pandoc \
 	--pdf-engine=$(ENGINE) \
 	--resource-path=$(RESPATH) \
 	--output=$@ \
-	$(OPT) $(OPT_PDF) \
-	$(_META)/$(TEMPLATE).yaml
+	$(OPT) $(OPT_PDF) $(META)
 
 TEX = pandoc \
 	--from=$(FROM) \
 	--to=$(TO_TEX) \
 	--resource-path=$(RESPATH) \
 	--output=$@ \
-	$(OPT) $(OPT_TEX) \
-	$(_META)/$(TEMPLATE).yaml
+	$(OPT) $(OPT_TEX) $(META)
 
 OPEN = $(BROWSER) $@
 VIEW = open -a Preview $@
