914dd9d57d0679ef446bdb1e5944cc7e9e45d306
Bernhard Waldbrunner <vbwx01@gmail.com>
2018-06-25 01:24:46 +0200

    Replace university-specific makefile with general pandoc makefile

--- include/jku.mk
+++ include/jku.mk
@@ -1,32 +1,3 @@
-SHELL := /bin/bash
+include pandoc.mk
 
-_META := $(HOME)/.pandoc/meta
-_INC := $(HOME)/.pandoc/include
-
-META = $(_META)/$(TEMPLATE).yaml
-INC = $(dir $@)include
-RESPATH = $(dir $@):$(INC):$(_INC)
-ENGINE := xelatex
 TEMPLATE := jku
-TO_TEX := latex
-FROM := markdown
-
-PDF = pandoc \
-	--from=$(FROM) \
-	--to=$(TO_TEX) \
-	--template=$(TEMPLATE).tex \
-	--pdf-engine=$(ENGINE) \
-	--resource-path=$(RESPATH) \
-	--output=$@ \
-	$(OPT) $(OPT_PDF) $(META)
-
-TEX = pandoc \
-	--from=$(FROM) \
-	--to=$(TO_TEX) \
-	--resource-path=$(RESPATH) \
-	--output=$@ \
-	$(OPT) $(OPT_TEX) $(META)
-
-OPEN = $(BROWSER) $@
-VIEW = open -a Preview $@
-EDIT = $(VISUAL) $@
---
+++ include/pandoc.mk
@@ -0,0 +1,31 @@
+SHELL := /bin/bash
+
+_META := $(HOME)/.pandoc/meta
+_INC := $(HOME)/.pandoc/include
+
+META = $(_META)/$(TEMPLATE).yaml
+INC = $(dir $@)include
+RESPATH = $(dir $@):$(INC):$(_INC)
+ENGINE := xelatex
+TO_TEX := latex
+FROM := markdown
+
+PDF = pandoc \
+	--from=$(FROM) \
+	--to=$(TO_TEX) \
+	--template=$(TEMPLATE).tex \
+	--pdf-engine=$(ENGINE) \
+	--resource-path=$(RESPATH) \
+	--output=$@ \
+	$(OPT) $(OPT_PDF) $(META)
+
+TEX = pandoc \
+	--from=$(FROM) \
+	--to=$(TO_TEX) \
+	--resource-path=$(RESPATH) \
+	--output=$@ \
+	$(OPT) $(OPT_TEX) $(META)
+
+OPEN = $(BROWSER) $@
+VIEW = open -a Preview $@
+EDIT = $(VISUAL) $@
